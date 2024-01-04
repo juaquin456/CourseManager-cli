4fc40f4cdc4dacfe9ca9d012ba898b835f0f01ce
juaquin456 <juaquin.remon@utec.edu.pe>
2024-01-03 19:27:14 -0500

    create parser structure

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,41 @@
+use clap::Parser;
+
 mod models;
+mod parser;
 
 fn main() {
+    let cli = parser::Cli::parse();
 
+    match cli.command {
+        parser::Commands::List { entity } => {
+            match entity {
+                parser::Entities::Cycles => {
+                    println!("Listing cycles");
+                },
+                parser::Entities::Courses => {
+                    println!("Listing courses");
+                }
+            }
+        },
+        parser::Commands::Create {entity} => {
+            match entity {
+                parser::Entity::Cycle(cycle) => {
+                    println!("Creating cycle {}-{}", cycle.age, cycle.semester);
+                },
+                parser::Entity::Course(course) => {
+                    println!("Creating course {}-{} {}", course.age, course.semester, course.name);
+                }
+            }
+        },
+        parser::Commands::Remove {entity} => {
+            match entity {
+                parser::Entity::Cycle(cycle) => {
+                    println!("Removing cycle {}-{}", cycle.age, cycle.semester);
+                },
+                parser::Entity::Course(course) => {
+                    println!("Removing course {}-{} {}", course.age, course.semester, course.name);
+                }
+            }
+        },
+    }
 }
---
+++ src/parser.rs
@@ -0,0 +1,58 @@
+use clap::{Args, Parser, Subcommand, ValueEnum};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Args, Clone)]
+pub struct Cycle {
+    pub age: u16,
+    pub semester: u8,
+}
+
+#[derive(Args)]
+pub struct Course {
+    pub age: u16,
+    pub semester: u8,
+
+    pub name: String,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    Create {
+        #[command(subcommand)]
+        entity: Entity,
+    },
+    Remove {
+        #[command(subcommand)]
+        entity: Entity
+    },
+    List {
+        #[arg(value_enum)]
+        entity: Entities
+    },
+    Go {
+        #[command(subcommand)]
+        entity: Entity,
+    },
+    Summary {
+        #[command(subcommand)]
+        entity: Entity,
+    },
+}
+
+#[derive(ValueEnum, Copy, Clone)]
+pub enum Entities {
+    Cycles,
+    Courses,
+}
+
+#[derive(Subcommand)]
+pub enum Entity {
+    Cycle(Cycle),
+    Course(Course),
+}
