6600e5eda152cae603c3ffc8dbf7782cdf466232
juaquin456 <juaquin.remon@utec.edu.pe>
2024-01-18 23:58:37 -0500

    add: new unit test for parsing commands

--- src/main.rs
+++ src/main.rs
@@ -205,3 +205,78 @@ fn main() {
         },
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use clap::Parser;
+    use crate::parser::{Cli, Commands, Entity, Entities};
+
+    #[test]
+    fn test_list_cycles() {
+        let cli = Cli::parse_from(["author", "list", "cycles"]);
+        match cli.command {
+            Commands::List { entity } => match entity {
+                Entities::Cycles => {}
+                _ => panic!("Wrong entity"),
+            },
+            _ => panic!("Wrong command"),
+        }
+    }
+
+    #[test]
+    fn test_list_courses() {
+        let cli = Cli::parse_from(["author", "list", "courses"]);
+        match cli.command {
+            Commands::List { entity } => match entity {
+                Entities::Courses => {}
+                _ => panic!("Wrong entity"),
+            },
+            _ => panic!("Wrong command"),
+        }
+    }
+
+    #[test]
+    fn test_create_cycle() {
+        let cli = Cli::parse_from(["author", "create", "cycle", "1", "2"]);
+        match cli.command {
+            Commands::Create { entity } => match entity {
+                Entity::Cycle(cycle) => {
+                    assert_eq!(cycle.age, 1);
+                    assert_eq!(cycle.semester, 2);
+                }
+                _ => panic!("Wrong entity"),
+            },
+            _ => panic!("Wrong command"),
+        }
+    }
+
+    #[test]
+    fn test_create_course() {
+        let cli = Cli::parse_from(["author", "create", "course", "1-2", "course_name"]);
+        match cli.command {
+            Commands::Create { entity } => match entity {
+                Entity::Course(course) => {
+                    assert_eq!(course.cycle_id, "1-2");
+                    assert_eq!(course.name, "course_name");
+                }
+                _ => panic!("Wrong entity"),
+            },
+            _ => panic!("Wrong command"),
+        }
+    }
+
+    #[test]
+    fn test_remove_cycle() {
+        let cli = Cli::parse_from(["author", "remove", "cycle", "1", "2"]);
+        match cli.command {
+            Commands::Remove { entity } => match entity {
+                Entity::Cycle(cycle) => {
+                    assert_eq!(cycle.age, 1);
+                    assert_eq!(cycle.semester, 2);
+                }
+                _ => panic!("Wrong entity"),
+            },
+            _ => panic!("Wrong command"),
+        }
+    }
+}
